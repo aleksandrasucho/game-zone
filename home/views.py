7b7656a8fd370adcd6e804261c1ce31b1b7b2ae6
aleksandrasucho <aleksandrasucho@gmail.com>
2024-02-22 16:44:29 +0000

    Update base.html and add home.html template to home app

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class HomeView(View):
+    """View for the home page."""
+    def get(self, request):
+        """Return the home page."""
+        return render(request, 'home.html')
