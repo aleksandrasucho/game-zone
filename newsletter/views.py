70314513aad25da0869ea14c62cbaf3a205923e3
aleksandrasucho <aleksandrasucho@gmail.com>
2024-03-11 15:30:16 +0000

    Add comments to views.py in newsletter app

--- newsletter/views.py
+++ newsletter/views.py
@@ -3,12 +3,14 @@ from .forms import SubscriberForm
 from .models import Subscriber
 
 def subscribe(request):
+    """View for subscribing to newsletter."""
     if request.method == 'POST':
         form = SubscriberForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
             # Check if the email already exists in the database
             if not Subscriber.objects.filter(email=email).exists():
+                # If email doesn't exist, create a new subscriber
                 subscriber = Subscriber(email=email)
                 subscriber.save()
             return redirect('success')
@@ -17,4 +19,5 @@ def subscribe(request):
     return render(request, 'subscribe.html', {'form': form})
 
 def success(request):
+    """View for displaying subscription success page."""
     return render(request, 'success.html')
