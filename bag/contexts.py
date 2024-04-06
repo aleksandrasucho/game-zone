e099f7194265c8ad60127a6199079579cbb640fd
aleksandrasucho <aleksandrasucho@gmail.com>
2024-04-06 16:49:00 +0100

    Add bag context processor for displaying shopping bag contents.

---
+++ bag/contexts.py
@@ -0,0 +1,25 @@
+from django.shortcuts import get_object_or_404
+from stock.models import Product
+
+def bag_contents(request):
+    """Return the contents of the shopping bag."""
+    bag_items = []
+    total = 0
+    product_count = 0
+    bag = request.session.get('bag', [])
+
+    if bag:
+        for product_id in bag:
+            product = get_object_or_404(Product, pk=product_id)
+            total += product.price
+            product_count += 1
+            bag_items.append({
+                'product': product,
+            })
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+    }
+    return context
